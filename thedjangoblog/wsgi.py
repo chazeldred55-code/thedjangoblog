a602aabd90988543774b83668db3d52e2d6458fc
Coolafdood <chazeldred55@gmail.com>
2025-11-18 12:26:04 +0000

    Fix unterminated string in wsgi.py

--- thedjangoblog/wsgi.py
+++ thedjangoblog/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thedjangoblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_django_blog.settings')
+
 
 
 application = get_wsgi_application()
