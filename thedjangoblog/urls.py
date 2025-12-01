1d62f387ad2eb2c16ed7493ffaa38ec597308aee
Coolafdood <chazeldred55@gmail.com>
2025-12-01 13:37:25 +0000

    Add collected static files to STATIC_ROOT to fix Summernote 404 errors

--- thedjangoblog/urls.py
+++ thedjangoblog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include  # include allows app-level URL routing
 
 urlpatterns = [
     path('admin/', admin.site.urls),   # Django admin
+    path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls')),    # All blog-related routes handled in blog/urls.py
 ]
