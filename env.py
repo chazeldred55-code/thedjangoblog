23d7c29ecb955c8cfa0e4182664092f7d718c6a7
Coolafdood <chazeldred55@gmail.com>
2025-12-16 10:58:09 +0000

    Storing images on Cloudinary so that our superuser can upload an image to go with each of their blog posts.

--- env.py
+++ env.py
@@ -11,3 +11,5 @@ os.environ.setdefault(
     'DATABASE_URL',
     'postgresql://neondb_owner:npg_dl0CytY9Mpna@ep-delicate-unit-ag1rq77b.c-2.eu-central-1.aws.neon.tech/god_music_scare_662035'
 )
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@dtkzkxpsk")
--- thedjangoblog/settings.py
+++ thedjangoblog/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'cloudinary_storage',
     "django.contrib.sites",
 
     # Third-party apps
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "django_summernote",
+    'cloudinary',
 
     # Local apps
     "about",
