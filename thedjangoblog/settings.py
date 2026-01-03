f96658aac6ab68102d70e3f3b6888fd3b0be670c
Coolafdood <chazeldred55@gmail.com>
2026-01-03 12:43:41 +0000

    Enable error logging for Heroku debugging

--- thedjangoblog/settings.py
+++ thedjangoblog/settings.py
@@ -205,3 +205,17 @@ SUMMERNOTE_CONFIG = {
 # Default PK
 # ------------------------
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
