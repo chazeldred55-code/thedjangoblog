e493a04cb6ff4201f7c7d08c568b72c0e87582ee
Coolafdood <chazeldred55@gmail.com>
2026-01-05 18:23:04 +0000

    Fix favicon redirect in urls.py

--- thedjangoblog/urls.py
+++ thedjangoblog/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
 
 urlpatterns = [
+    # Favicon redirect
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=settings.STATIC_URL + "images/favicon.ico"),
+        name="favicon",
+    ),
 
     # Local apps (alphabetical)
     path("about/", include("about.urls"), name="about-urls"),
