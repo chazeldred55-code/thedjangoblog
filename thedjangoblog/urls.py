1747d2139c16a01d3b53609d191dd85724bfb89b
Coolafdood <chazeldred55@gmail.com>
2026-01-14 14:47:02 +0000

    Fix home routing and add /home/ alias

--- thedjangoblog/urls.py
+++ thedjangoblog/urls.py
@@ -4,18 +4,25 @@ from django.views.generic import RedirectView
 from django.conf import settings
 
 urlpatterns = [
-    # Home
+    # Home (named) + /home/ alias
     path("", RedirectView.as_view(pattern_name="blog_home", permanent=False), name="home"),
-    
-    path("favicon.ico", RedirectView.as_view(url=settings.STATIC_URL + "images/favicon.ico"), name="favicon"),
+    path("home/", RedirectView.as_view(pattern_name="home", permanent=False)),
 
+    # Favicon redirect
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=settings.STATIC_URL + "images/favicon.ico"),
+        name="favicon",
+    ),
+
+    # Local apps
     path("about/", include("about.urls")),
-    path("blog/", include("blog.urls")),  # homepage now at /blog/
+    path("blog/", include("blog.urls")),
 
+    # Third-party apps
     path("accounts/", include("allauth.urls")),
     path("summernote/", include("django_summernote.urls")),
-    path("admin/", admin.site.urls),
 
-    # Redirect / to /blog/ so homepage works
-    path("", RedirectView.as_view(url="/blog/", permanent=False)),
+    # Django admin
+    path("admin/", admin.site.urls),
 ]
