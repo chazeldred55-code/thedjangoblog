8af37ef8fb1e606ebbaa7867f025e115200d1269
chazeldred55-code <chazeldred55@gmail.com>
2026-04-16 11:32:33 +0100

    Fix persistent aria-describedby validation error by overriding password widgets in CustomSignupForm

--- blog/forms.py
+++ blog/forms.py
@@ -12,10 +12,20 @@ def **init**(self, *args, **kwargs):
 super().**init**(*args, **kwargs)
 
 ```
-    # Remove invalid aria-describedby attributes (fixes W3C validation error)
+    # Force override widgets to remove aria-describedby AFTER allauth processing
     if 'password1' in self.fields:
-        self.fields['password1'].widget.attrs.pop('aria-describedby', None)
+        self.fields['password1'].widget = forms.PasswordInput(
+            attrs={
+                'placeholder': 'Password',
+                'autocomplete': 'new-password',
+            }
+        )
 
     if 'password2' in self.fields:
-        self.fields['password2'].widget.attrs.pop('aria-describedby', None)
+        self.fields['password2'].widget = forms.PasswordInput(
+            attrs={
+                'placeholder': 'Confirm Password',
+                'autocomplete': 'new-password',
+            }
+        )
 ```
