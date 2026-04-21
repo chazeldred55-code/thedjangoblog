161edbf2343a6f2cea594cf6fe832adec9639c7a
chazeldred55-code <chazeldred55@gmail.com>
2026-04-21 14:08:31 +0100

    fix: remove conflicting tests.py and add working view tests

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,23 +1,70 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from blog.models import Post, Comment, Category
 
-from ..forms import CommentForm
 
+class TestCommentViews(TestCase):
 
-class TestCommentForm(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.owner = User.objects.create_user(
+            username='owner', password='testpass123')
+        self.other = User.objects.create_user(
+            username='other', password='testpass123')
 
-    def test_form_is_valid(self):
-        form = CommentForm(
-            {
-                "body": "This is a test comment",
-            }
+        self.category = Category.objects.create(name='Test')
+
+        self.post = Post.objects.create(
+            title='Test Post',
+            slug='test-post',
+            author=self.owner,
+            category=self.category,
+            content='Test content',
+            status=1
         )
-        self.assertTrue(form.is_valid())
 
-    def test_form_is_invalid(self):
-        form = CommentForm(
-            {
-                "body": "",
-            }
+        self.comment = Comment.objects.create(
+            post=self.post,
+            author=self.owner,
+            body='Test comment',
+            approved=True
         )
-        self.assertFalse(form.is_valid())
-        self.assertIn("body", form.errors)
+
+    def test_unauthenticated_cannot_edit_comment(self):
+        url = reverse('comment_edit', args=[
+            self.post.slug, self.comment.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('/accounts/login', response['Location'])
+
+    def test_unauthenticated_cannot_delete_comment(self):
+        url = reverse('comment_delete', args=[
+            self.post.slug, self.comment.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('/accounts/login', response['Location'])
+
+    def test_non_owner_cannot_edit_comment(self):
+        self.client.login(username='other', password='testpass123')
+        url = reverse('comment_edit', args=[
+            self.post.slug, self.comment.id])
+        response = self.client.post(url, {'body': 'hacked'})
+        self.assertEqual(response.status_code, 302)
+
+    def test_non_owner_cannot_delete_comment(self):
+        self.client.login(username='other', password='testpass123')
+        url = reverse('comment_delete', args=[
+            self.post.slug, self.comment.id])
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(
+            Comment.objects.filter(id=self.comment.id).exists()
+        )
+
+    def test_owner_can_edit_comment(self):
+        self.client.login(username='owner', password='testpass123')
+        url = reverse('comment_edit', args=[
+            self.post.slug, self.comment.id])
+        response = self.client.post(url, {'body': 'updated'})
+        self.assertEqual(response.status_code, 302)
